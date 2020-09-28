9936f284f8c3abb0cb3a451fd7a662774031ca24
guillemsunyer-omnidrone <guillem.sunyer@omnidrone.net>
2020-09-28 19:42:02 +0200

    Working on tests

--- Tests/TransformTests.cs
+++ Tests/TransformTests.cs
@@ -14,10 +14,9 @@ public class TransformTests : MonoBehaviour
     {
         sequenceTween = new SequenceTween();
 
-        //sequenceTween.Join(toTween.TweenRotation(new Vector3(0.0f, 0.0f, 90.0f), 1));
         sequenceTween.Append(toTween.TweenPosition(new Vector2(1, 0), 1));
-        //sequenceTween.Join(toTween.TweenLocalScale(new Vector2(2, 2), 4));
-        //sequenceTween.Join(toTween.TweenRotation(new Vector3(0.0f, 0.0f, 360.0f), 4));
+        sequenceTween.Join(toTween.TweenLocalScale(new Vector2(2, 2), 4));
+        sequenceTween.Join(toTween.TweenRotation(new Vector3(0.0f, 0.0f, 360.0f), 4));
         sequenceTween.Append(toTween.TweenPosition(new Vector2(-1, 0), 1));
 
         sequenceTween.SetEase(animationCurve);
